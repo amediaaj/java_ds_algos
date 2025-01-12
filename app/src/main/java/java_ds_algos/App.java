c489c0d4261fc7f4d89267c62113732221eb47f4
Alex Amedia <aamedia@asu.edu>
2025-01-12 12:20:30 -0700

    Linked list - Description

--- app/src/main/java/java_ds_algos/App.java
+++ app/src/main/java/java_ds_algos/App.java
@@ -102,8 +102,30 @@ public class App {
 
      /******************************************************
      * 
-     * Linked List =  
+     * Linked List =  stores Nodes in 2 parts (data + address)
+     *                Nodes are in non-consecutive memory locations
+     *                Elements are linked using pointers.
      * 
+     * Singly Linked List
+     * [data | address] -> [data | address] -> [data | address]
+     * 
+     * Doubly Linked List
+     * [address | data | address] <-> [address | data | address]
+     * 
+     * advantages
+     * 1. Dynamic Data Structure (allocates needed memory while running)
+     * 2. Insertion and Deltion of Nodes is easy. O(1)
+     * 3. No/Low memory waste
+     * 
+     * disadvantages
+     * 1. Greater memory usage (additional pointer)
+     * 2. No random access of elements (no index [i])
+     * 3. Accessing/search elements is more time consuming. O(n).
+     *
+     * uses
+     * 1. Implement staacks/queues
+     * 2. GPS navigation
+     * 3. music playlist 
      ******************************************************/
     public void testLinkedList() {
         System.out.println("Testing linked list...");
